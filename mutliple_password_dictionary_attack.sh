630ae9d14463432985c3bdede240fb769672b5cc
Rana Salal Ali <ranasalal@gmail.com>
2020-12-17 12:22:05 +1100

    bash

--- mutliple_password_dictionary_attack.sh
+++ mutliple_password_dictionary_attack.sh
@@ -2,8 +2,8 @@
 
 
 #DICTIONARY ATTACK
-python generate_r_space.py --r_space 10000 --type password --secret qwertyui
-python pick_N_r_space_passwords.py --r_space 1000 --N $1
+python generate_r_space.py --r_space $2 --type password --secret qwertyui --knowledge 2 --dictionary n
+python pick_N_r_space_passwords.py --r_space $2 --N $1
 
 filename="r_space_data/$1_r_space_passwords.txt"
 n=1
@@ -14,7 +14,9 @@ declare -i password_len=${#line}
 declare -i start_loc=17
 declare -i end_loc=start_loc+password_len
 
-qsub -v password=$password,start_loc=$start_loc,end_loc=$end_loc,run=$n,n_passwords=$1 jobscript;
+echo "Rana's secret is "$password""
+
+qsub -v password=$password,start_loc=$start_loc,end_loc=$end_loc,run=$n,n_passwords=$1,r_space=$2 jobscript;
 
 n=$((n+1))
 done < $filename
