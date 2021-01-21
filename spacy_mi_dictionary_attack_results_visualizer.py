a9670f35137cc4b7ae98df2d5cd5160dcd929be4
Rana Salal Ali <ranasalal@gmail.com>
2021-01-21 15:50:54 +1100

    averaged epoch rank over passwords

--- spacy_mi_dictionary_attack_results_visualizer.py
+++ spacy_mi_dictionary_attack_results_visualizer.py
@@ -143,6 +143,29 @@ if __name__ == "__main__":
         password_Stat[secret] = PasswordStats(secret)
 
 
+    #FIGURE 0 - RANK PER EPOCH/INSERTIONS
+
+    plt.figure()
+
+    first_key = list(avg_epoch_rank_per_password.keys())[0]
+    overall_avg_epoch_rank = {epoch:{} for epoch in avg_epoch_rank_per_password[first_key].keys()}
+
+    for i in avg_epoch_rank_per_password:
+        for epoch, rank in avg_epoch_rank_per_password.items():
+            overall_avg_epoch_rank[epoch].append(rank)
+        
+    for key in overall_avg_epoch_rank:
+        overall_avg_epoch_rank[key] = np.mean(np.array(overall_avg_epoch_rank[key]))
+
+    
+    plt.xlabel('Epochs')
+    plt.ylabel('Ranks')
+    plt.plot(overall_avg_epoch_rank.keys(), overall_avg_epoch_rank.values())
+    plt.legend()
+    plt.tight_layout()
+    plt_dest = plt_folder + 'OVERALL_AVERAGE_RANK_PER_EPOCH_OR_INSERTION'
+    plt.savefig(plt_dest,
+            bbox_inches="tight")
 
     #FIGURE 1 - RANK PER EPOCH/INSERTIONS
 
@@ -161,6 +184,7 @@ if __name__ == "__main__":
     plt.savefig(plt_dest,
             bbox_inches="tight")
 
+    
 
     #FIGURE 2 - DIGITS vs LETTERS EXPOSURE RANK
 
