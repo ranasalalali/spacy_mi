a33cb927c2873bb4ea33e2e3ea79c5f30e6861f0
tha061 <tham.nguyen@mq.edu.au>
2021-03-01 10:15:34 +1100

    add in out vocab check

---
+++ in_out_vocab_attack.py
@@ -0,0 +1,76 @@
+from __future__ import unicode_literals, print_function
+import spacy
+print(spacy.__version__)
+from collections import defaultdict
+from thinc.api import set_gpu_allocator,require_gpu
+
+
+from spacy.training import Example
+import random
+import warnings
+from pathlib import Path
+import spacy
+from spacy.util import minibatch, compounding
+from collections import defaultdict
+import operator
+import string
+import numpy as np
+from itertools import permutations, islice
+import os
+import errno
+import multiprocessing as mp
+from spacy.vectors import Vectors
+import time
+import sys
+
+file_name = open("in_out_vocab.txt","a")
+
+nlp = spacy.load('en_core_web_lg')
+
+iterations = 100
+for i in range(iterations):
+    ## in vocab
+    print("-----IN vocab-----")
+    vocab_string_org = list(nlp.vocab.strings)
+    print("len of vocab before query {}".format(len(vocab_string_org)))
+    
+    text = "password"
+    
+    time0 = time.perf_counter()
+    doc = nlp(text)
+    time_now = time.perf_counter()
+    vocab_string_after_query = list(nlp.vocab.strings)
+    in_vocab_runtime = time_now - time0
+    total_in_vocab_time += in_vocab_runtime
+
+    print("len of vocab before query {}".format(len(vocab_string_after_query)))
+
+    ## out vocab
+    print("-----OUT vocab-----")
+    vocab_string_org = list(nlp.vocab.strings)
+    print("len of vocab before query {}".format(len(vocab_string_org)))
+    
+    text = "dfjgkkd908lkg"
+    
+    time0 = time.perf_counter()
+    doc = nlp(text)
+    time_now = time.perf_counter()
+    vocab_string_after_query = list(nlp.vocab.strings)
+    out_vocab_runtime = time_now - time0
+    total_out_vocab_time += out_vocab_runtime
+
+    print("len of vocab before query {}".format(len(vocab_string_after_query)))
+
+    if out_vocab_runtime > in_vocab_runtime:
+        count_success +=1
+    # print("-------------------")
+
+file_name.write("Number of successs attempts:{}\n".format(count_success))    
+file_name.write("======Average======\n") 
+if iterations >0:
+    file_name.write("runtime with in vocab: {}\n".format(total_in_vocab_time/iterations))
+    file_name.write("runtime with out vocab: {}\n".format(total_out_vocab_time/iterations))
+    file_name.write("runtime diff: {}\n".format(total_out_vocab_time/iterations - total_in_vocab_time/iterations ))
+
+
+
