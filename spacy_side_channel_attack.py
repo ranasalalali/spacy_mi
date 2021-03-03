e85858e3ff1c1a212e1691c4b2bf2a74920fd121
tha061 <tham.nguyen@mq.edu.au>
2021-03-03 22:05:38 +1100

    test avg runtime in vocab

--- spacy_side_channel_attack.py
+++ spacy_side_channel_attack.py
@@ -806,7 +806,7 @@ def get_avg_runtime_in_vocab():
         vocab_string_org = list(nlp.vocab.strings)
         print("len of vocab before query {}".format(len(vocab_string_org)))
         
-        text = "Rana's secret is" + i
+        text = "Rana's secret is " + i
         print(text)
         
         ner = nlp.get_pipe('ner')
