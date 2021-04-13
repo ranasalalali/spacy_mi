1cf36ce6967570faec08c25619ec631e27703300
tha061 <tham.nguyen@mq.edu.au>
2021-04-13 12:47:50 +1000

    add code

--- mia_attaker.py
+++ mia_attaker.py
@@ -643,7 +643,7 @@ if __name__ == "__main__":
     num_test = 100
     vocab = list(nlp.vocab.strings)
     in_vocab_words = vocab[10000:10000+num_test]
-    in_vocab_words_test = vocab[12000:12000+num_test]
+    in_vocab_words_test = vocab[10000:10000+num_test]
     # in_vocab_words_test = ['news', 'people', 'the', 'you', 'home']
     # in_vocab_words_test = ['people', 'update', 'school','sample', 'random']
     # in_vocab_words_test = ['home', 'home', 'home', 'home', 'home']
