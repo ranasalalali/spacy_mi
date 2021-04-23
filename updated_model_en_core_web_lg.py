ca46a3d843794285449415850b8bb717924756c9
tha061 <nttham0611@gmail.com>
2021-04-23 00:35:38 +0000

    results

--- updated_model_en_core_web_lg.py
+++ updated_model_en_core_web_lg.py
@@ -288,7 +288,7 @@ if __name__ == "__main__":
 
     pws = g[:][0]
 
-    num_test = 2000
+    num_test = 1000
     updating_pws = pws[0:num_test]
     in_vocab_words_test = updating_pws
     out_vocab_words = pws[num_test:2*num_test]
