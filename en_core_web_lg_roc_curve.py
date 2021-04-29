ddbebead97215dc8d10a62ae0703c0b2cf8e112e
tha061 <tham.nguyen@mq.edu.au>
2021-04-29 14:28:42 +1000

    code

--- en_core_web_lg_roc_curve.py
+++ en_core_web_lg_roc_curve.py
@@ -261,7 +261,7 @@ if __name__ == "__main__":
 
     # nlp = spacy.load("en_core_web_lg")
     # global vocab
-    num_test = 500
+    num_test = 1000
     # vocab = list(nlp.vocab.strings)
     # in_vocab_words = vocab[10000:10000+num_test]
     vocab = vocab_lg
